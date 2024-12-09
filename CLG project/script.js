6904cb43300d2d503f755a5ee6c9681a8620f246
Aswinraj <aaswin76862@gmail.com>
2024-12-09 22:39:56 +0530

    new commit

---
+++ CLG project/script.js
@@ -0,0 +1,15 @@
+// Basic form validation example
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    
+    if (name && email && message) {
+        alert("Your message has been sent! We will get back to you soon.");
+        document.getElementById("contact-form").reset();
+    } else {
+        alert("Please fill in all the fields.");
+    }
+});
